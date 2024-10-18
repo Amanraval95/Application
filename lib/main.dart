17a8ac1bbb97ebbbd42838d86be36cc8a02b485a
Amanraval95 <86317761+Amanraval95@users.noreply.github.com>
2024-10-18 15:31:43 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:new_app/pages/cart_page.dart';
 import 'package:new_app/utils/routes.dart';
-import 'package:new_app/pages/cart_page.dart';
-import 'package:new_app/utils/routes.dart';
-import 'theme.dart';
-import 'pages/login_page.dart';
+
 import 'pages/home_page.dart';
+import 'pages/login_page.dart';
+import 'theme.dart';
 
 void main() {
   runApp(myfirstapp());
 }
 
 class myfirstapp extends StatelessWidget {
-   myfirstapp({super.key});
-  var lightColorTheme=MaterialTheme.lightScheme();
-   var darkColorTheme=MaterialTheme.darkScheme();
+  myfirstapp({super.key});
+  var lightColorTheme = MaterialTheme.lightScheme();
+  var darkColorTheme = MaterialTheme.darkScheme();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       theme: ThemeData(colorScheme: lightColorTheme),
-      darkTheme:ThemeData(colorScheme: darkColorTheme), // ThemeData
+      darkTheme: ThemeData(colorScheme: darkColorTheme), // ThemeData
       initialRoute: MyRoutes.Homeroute,
       routes: {
         // "/": (context) => Home_page(),
         MyRoutes.Loginroute: (context) => loginpage(),
         MyRoutes.Homeroute: (context) => Home_page(),
         MyRoutes.CartRoute: (context) => CartPage()
+        //heyyy
       },
     ); // MaterialApp
   }
